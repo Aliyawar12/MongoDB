3c2ca8e4256c9831717329269021b6cced5da65f
Muhammad Yawar Ali <141340403+Aliyawar12@users.noreply.github.com>
2024-07-26 12:42:41 +0500

    Update express-app.js

--- BackEnd/express-app.js
+++ BackEnd/express-app.js
@@ -7,7 +7,7 @@ const userRoutes = require("./Routes/userRoute");
 const ArticleRoutes = require("./Routes/ArticleRoute");
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || //Mention Port Here;
 
 connectDB();
 
